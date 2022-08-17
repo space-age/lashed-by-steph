b086ddfff382465de8e839907c85f353a6722606
space-age <53498728+space-age@users.noreply.github.com>
2022-08-17 11:56:22 -0700

    removed crossoirigin

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <title>Lashed by Steph</title>
         <link rel="icon" href="/tab-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
           rel="stylesheet"
